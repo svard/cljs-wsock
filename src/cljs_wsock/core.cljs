e61072fef80533d55bd4f8b0035e471a13b219ae
Kristofer Svärd <kristofer.svard@gmail.com>
2014-01-04 20:36:43 +0100

    init function
    The init function instansiate a new WebSocket object and sets up listeners for it's events.

--- src/cljs_wsock/core.cljs
+++ src/cljs_wsock/core.cljs
@@ -5,52 +5,70 @@
   (:import [goog.net WebSocket]
            [goog.net.WebSocket EventType]))
 
-(def ^:private socket (WebSocket.))
+(def ^:private socket (atom 0))
 
 (def ^:private channel (chan))
 
 (defn ^:private listen
   "Sets up event listeners for the websocket connection"
   []
-  (events/listen socket EventType/OPENED (fn [e]
+  (events/listen @socket EventType/OPENED (fn [e]
                                            (put! channel [:opened e])))
-  (events/listen socket EventType/MESSAGE (fn [e]
+  (events/listen @socket EventType/MESSAGE (fn [e]
                                             (put! channel [:message e])))
-  (events/listen socket EventType/CLOSED (fn [e]
+  (events/listen @socket EventType/CLOSED (fn [e]
                                            (put! channel [:closed e])))
-  (events/listen socket EventType/ERROR (fn [e]
+  (events/listen @socket EventType/ERROR (fn [e]
                                           (put! channel [:error e]))))
+
+(defn init
+  "Initializes a new web socket connection
+
+  auto-reconnect - boolean, optional, should the web socket automatically try to reconnect.
+                   True by default.
+
+  get-next-reconnect - number or fn, optional, function for obtaining the time until next
+                       reconnect attempt. fn will be passed the reconnect attempt
+                       count and should return a positive integer representing the
+                       time in milliseconds until next reconnect attempt.
+  "
+  ([]
+   (init nil nil))
+  ([auto-reconnect]
+   (init auto-reconnect nil))
+  ([auto-reconnect get-next-reconnect]
+   (reset! socket (WebSocket. auto-reconnect get-next-reconnect))
+   (listen)))
 
 (defn open
   "Opens a connection to the specified url. Returns a channel that will
   receive a vector [type event] where type can be :opened, :message,
   :closed or :error
 
-  url - a string specifying which server to connect to
+  url - string, specifying which server to connect to
 
-  protocol - optional, string describing what subprotocol to use
+  protocol - string, optional, describing what subprotocol to use
   "
   ([url]
     (open url nil))
   ([url protocol]
-    (listen)
-    (.open socket url protocol)
+    (.open @socket url protocol)
     channel))
 
 (defn send
   "Sends a message on an open web socket connection
 
-  message - string to send
+  message - string, message to send
   "
   [message]
-  (.send socket message))
+  (.send @socket message))
 
 (defn close
   "Close an open web socket connection"
   []
-  (.close socket))
+  (.close @socket))
 
 (defn open?
   "Checks to see if the web socket is open. Returns a boolean"
   []
-  (.isOpen socket))
+  (.isOpen @socket))
